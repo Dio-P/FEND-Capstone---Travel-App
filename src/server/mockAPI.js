ff34bc8afbd4eb1c6df514b22182cda729c6f1b8
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-09-15 13:42:38 +0100

    the link produced before the call is correct and gives results, I am about to activate axios now, hoping to see it all in the console

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,24 +1,22 @@
 const axios = require('axios').default;
 require('dotenv').config()
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 let inputBox = {}
 
 async function apiCall(req, res){
-    console.log("url.req.bodyis: ");
-    console.log(req.body);
-    const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?key=";
+    // console.log("url.req.bodyis: ");
+    // console.log(req.body);
+    const baseUrl = "http://api.geonames.org/postalCodeSearch?placename=" 
     console.log(baseUrl);
-    const apiKey = process.env.apiKey;
-    console.log("apiKey =>", apiKey);
-    const ofType = "&of=json&url=";
-    const lang = "&lang=en";
-    let nUrl = req.body.data.newUrl;
-    console.log("url =>", nUrl);
-    const requestOptions=  {
-      method: 'POST',
-      redirect: 'follow'
-    };
-    let url = (baseUrl+apiKey+ofType+nUrl+lang);
+    const placename = "Glasgow";
+    // const apiKey = process.env.apiKey;
+    // console.log("apiKey =>", apiKey);
+    const country = "&country=GB"
+    const maxRows = "&maxRows=10";
+    // let nUrl = req.body.data.newUrl;
+    // console.log("url =>", nUrl);
+    const username = "&username=dio_papa";
+    let url = (baseUrl+placename+country+maxRows+username);
     console.log("url =>", url);
         let options = {
             method: 'GET',
@@ -28,21 +26,22 @@ async function apiCall(req, res){
                 'Content-Type': 'application/json;charset=UTF-8'
             },
         };
-const response = await axios(options, requestOptions);
-// let responseOK = response && response.status === 200 && response.statusText === 'OK';
-// if (responseOK) {
-    console.log("response is =>", response);
-    let data = await response.data;
+    }
+// const response = await axios(options, requestOptions);
+// // let responseOK = response && response.status === 200 && response.statusText === 'OK';
+// // if (responseOK) {
+//     console.log("response is =>", response);
+//     let data = await response.data;
     
-    // console.log("data is =>", data);
-    inputBox["agreement"]=data.agreement;
-    inputBox["subjectivity"]=data.subjectivity;
-    inputBox["confidence"]=data.confidence;
-    inputBox["irony"]=data.irony;
-    console.log(inputBox);
-    return inputBox
-    // do something with data
-}
+//     // console.log("data is =>", data);
+//     inputBox["agreement"]=data.agreement;
+//     inputBox["subjectivity"]=data.subjectivity;
+//     inputBox["confidence"]=data.confidence;
+//     inputBox["irony"]=data.irony;
+//     console.log(inputBox);
+//     return inputBox
+//     // do something with data
+// }
     // }
 //     const responce = async (baseUrl, apiKey, ofType, url, lang, requestOptions) =>{
 //       await fetch(baseUrl+apiKey+ofType+url+lang, requestOptions)
@@ -75,4 +74,3 @@ const response = await axios(options, requestOptions);
     method:apiCall,
     otherMethod: inputBox,
     /*otherOtherMethod: baseUrl*/}
-// module.exports = apiCall
