7c6261cfa64420b109da272126a5628ed41ef247
Dionisis Papadopoulos <82506233+Dio-P@users.noreply.github.com>
2021-09-17 17:33:59 +0100

    the count down is working. I need now to fix the bag with the object, and to make the if for the swap of prognosis + styles

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 let UI_Inp = {}
 new Litepicker({
     element: document.getElementById('datepicker'),
-    singleMode: false,
+    singleMode: true,
     tooltipText: {
       one: 'night',
       other: 'nights'
@@ -15,7 +15,7 @@ new Litepicker({
       return totalDays - 1;
     }
   })
-
+ 
 async function handleSubmit(event) {
     // const form = document.getElementById('form');
     // form.addEventListener('submit', logSubmit, true);
@@ -28,24 +28,59 @@ async function handleSubmit(event) {
     let formDate= document.getElementById('datepicker').value
     console.log("formDate =>", formDate);
 
-    let date = formDate.split(' ');
-    console.log(date);
-    let startingDate = date[0];
-    console.log("startingDate =>", startingDate);
-    let endData = date[2];
-    console.log("endData =>", endData);
-  
+    // let date = formDate.split(' ');
+    // console.log(date);
+    // let startingDate = date[0];
+    // console.log("startingDate =>", startingDate);
+    // let endData = date[2];
+    // console.log("endData =>", endData);
 
     let formCity= document.getElementById("city").value;
     console.log("formCity=>", formCity);
     alert(UI_Inp);
-    // you need to send all to the server, in one, or seperate objects !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+    const countDownDate = new Date(formDate).getTime(); // input from the UI. but in what form
+
+    // Run myfunc every second
+    const myfunc = setInterval(function() {
+
+    const now = new Date().getTime();
+  
+    // console.log("now =>", now);
+    // console.log("new Date().toString() =>", new Date().toString());
+
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    // var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    // var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + " days remaining until your trip"
+    // document.getElementById("hours").innerHTML = hours + "h " 
+    // document.getElementById("mins").innerHTML = minutes + "m " 
+    // document.getElementById("secs").innerHTML = seconds + "s " 
+        
+    // Display the message when countdown is over
+    // if (timeleft < 0) {
+    //     clearInterval(myfunc);
+    //     document.getElementById("days").innerHTML = ""
+    //     document.getElementById("hours").innerHTML = "" 
+    //     document.getElementById("mins").innerHTML = ""
+    //     document.getElementById("secs").innerHTML = ""
+    //     document.getElementById("end").innerHTML = "TIME UP!!";
+    // }
+    }, 1000);
+
+    // you need to send all to the server, in one, or seperate objects !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // CreateNURL(formText);
     // console.log("newUrl is !!!!!!!!!!!!!==>", newUrl);
     // UI_Inp["newUrl"]=newUrl;
     UI_Inp["formCity"]=formCity;
-    UI_Inp["formDate"]=formDate;
+    UI_Inp["startingDate"]=formDate;
+    // UI_Inp["endData"]=endData;
     console.log(UI_Inp);
     Client.postData("/UI_Inp", {UI_Inp})
 
@@ -74,7 +109,23 @@ const respons= await axios.get('http://localhost:3000/results')
         console.log("error", error);
     }
 }
+//  // let d = new Date();
+//   // let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
+//   // let countDownDate = new Date("Sep 5, 2018").getTime();
+// let countDown = setInterval(function() { 
 
+
+
+
+
+
+
+
+
+
+//   // let distance = countDownDate - now;      
+//   // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+// }
 
 // const respons= await fetch('http://localhost:3000/results')
 //     try {
@@ -88,4 +139,4 @@ const respons= await axios.get('http://localhost:3000/results')
 //     }
 // }
 
-export { handleSubmit, }
+export { handleSubmit }
